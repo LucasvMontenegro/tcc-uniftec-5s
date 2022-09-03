e633d0ecbead31fffd73d99d0f9160ffc75ace1c
Lucas Montenegro <lucas.montenegro@bavabank.com>
2022-09-03 15:55:37 -0300

    feat: add http server with health endpoint

---
+++ internal/interface/http/service.go
@@ -0,0 +1,30 @@
+package api
+
+import (
+	"github.com/labstack/echo/v4"
+
+	"github.com/tcc-uniftec-5s/internal/interface/http/handler"
+)
+
+type serviceImpl struct {
+	api  *echo.Echo
+	Port string
+}
+
+func NewService(
+	port string,
+	applicationName string,
+) *serviceImpl {
+	echoAPI := echo.New()
+
+	handler.MakeHealtHandler(echoAPI)
+
+	return &serviceImpl{
+		api:  echoAPI,
+		Port: port,
+	}
+}
+
+func (s serviceImpl) StartServer() error {
+	return s.api.Start(s.Port)
+}
