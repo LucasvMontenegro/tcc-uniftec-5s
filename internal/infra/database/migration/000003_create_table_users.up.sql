975e8a47bf987836ff252837640ee2544d1579e6
Lucas Montenegro <lucas.montenegro@bavabank.com>
2022-09-23 16:31:34 -0300

    fix: users, editions and teams migrations

--- internal/infra/database/migration/000003_create_table_users.up.sql
+++ internal/infra/database/migration/000003_create_table_users.up.sql
@@ -1,9 +1,12 @@
+DROP TYPE IF EXISTS user_status;
+CREATE TYPE user_status AS ENUM ('ACTIVE', 'INACTIVE');
+
 create table if not exists users (
 	id bigserial NOT NULL,
 	account_id BIGINT UNIQUE NOT NULL,
 	name varchar(255) NOT NULL,
 	is_admin boolean NOT NULL DEFAULT false,
-	status varchar(65) NOT NULL,
+	status user_status NOT NULL,
 	created_at timestamptz NOT NULL,
 	updated_at timestamptz NOT NULL,
 
--- internal/infra/database/migration/000007_create_table_editions.up.sql
+++ internal/infra/database/migration/000007_create_table_editions.up.sql
@@ -1,11 +1,14 @@
+DROP TYPE IF EXISTS edition_status;
+CREATE TYPE edition_status AS ENUM ('WAITING', 'ACTIVE', 'FINISHED');
+
 create table if not exists editions (
 	id bigserial NOT NULL,
 	winner_id BIGINT NULL DEFAULT NULL,
 	name varchar(65) NULL DEFAULT NULL,
 	description varchar(65) NULL DEFAULT NULL,
-	status varchar(65) NULL DEFAULT NULL,
+	status edition_status NOT NULL,
 	start_date timestamptz NOT NULL,
 	end_date timestamptz NOT NULL,
 
   CONSTRAINT edition_pkey PRIMARY KEY (id)
-  );
+);
