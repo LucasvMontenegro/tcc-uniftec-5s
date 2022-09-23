48227dc1228089b23ccf7bb5883c81fd9db22a2d
Lucas Montenegro <lucas.montenegro@bavabank.com>
2022-09-23 16:35:42 -0300

    feat: implement repository method

--- internal/app/repository/user.go
+++ internal/app/repository/user.go
@@ -42,3 +42,36 @@ func (r userRepository) Save(ctx context.Context, user *entity.User) error {
 	user.ID = userDS.ID
 	return err
 }
+
+func (r userRepository) GetTeamlessUsers(ctx context.Context) ([]*entity.User, error) {
+	dbconn := r.db
+	ctxValue, ok := ctx.Value(CtxKey{}).(CtxValue)
+	if ok {
+		dbconn = ctxValue.tx
+	}
+
+	teamlessUsersDS := []datastructure.User{}
+	err := dbconn.
+		WithContext(ctx).
+		Table("team_user").
+		Raw("select * from users u where u.id not in (select tu.user_id from team_user tu where tu.team_id in (select t.id from teams t where t.edition_id = (select e.id  from editions e where e.status = 'ACTIVE')))").
+		Scan(&teamlessUsersDS).
+		Error
+
+	teamlessUsers := []*entity.User{}
+	for _, tu := range teamlessUsersDS {
+		u := entity.User{
+			ID: tu.ID,
+			Account: &entity.Account{
+				ID: tu.AccountID,
+			},
+			Name:    *tu.Name,
+			IsAdmin: *tu.IsAdmin,
+			Status:  entity.UserStatusEnum(*tu.Status),
+		}
+
+		teamlessUsers = append(teamlessUsers, &u)
+	}
+
+	return teamlessUsers, err
+}
