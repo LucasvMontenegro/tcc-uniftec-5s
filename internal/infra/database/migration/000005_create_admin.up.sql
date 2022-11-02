f4a6d5a73ef63fb26b32cd6408e3b7b361d324cf
Lucas Montenegro <lucas.montenegro@bavabank.com>
2022-11-02 18:43:59 -0300

    fix: admin insertion

--- internal/infra/database/migration/000005_create_admin.up.sql
+++ internal/infra/database/migration/000005_create_admin.up.sql
@@ -1,8 +1,8 @@
-INSERT INTO credentials (id, account_id, email, password, created_at, updated_at) values (1, null, 'admin@admin.com', 'admin', now(), now());
+INSERT INTO credentials (account_id, email, password, created_at, updated_at) values (null, 'admin@admin.com', 'admin', now(), now());
 
-INSERT INTO accounts (id, credential_id, user_id, email, created_at, updated_at) VALUES (1,1,null,'admin@admin.com', now(), now());
+INSERT INTO accounts (credential_id, user_id, email, created_at, updated_at) VALUES (1,null,'admin@admin.com', now(), now());
 
-INSERT INTO users (id, account_id, name, is_admin, status, created_at, updated_at) values (1, 1, 'admin', true, 'ACTIVE', now(), now());
+INSERT INTO users (account_id, name, is_admin, status, created_at, updated_at) values (1, 'admin', true, 'ACTIVE', now(), now());
 
 UPDATE credentials SET account_id = 1 WHERE id = 1;
 
